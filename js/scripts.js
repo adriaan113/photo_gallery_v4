54f6967de75e6c39dac7013c13d8e5c6577a8454
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-07-09 15:03:46 +0200

    added javascript/ added alt text in html/ adjusted grid gaps

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
 
-let title;
-let test;
+// Jquery code: i wanred to coe this challenge with jquery but could not figure out how to make
+//the [data-title] toLowerCase.
 
-$('a').each(function(){
-  title= $(this).attr('data-title');
-//  title= title.toLowerCase();
-  console.log(title);
-});
+// let input;
+// let filter;
+//
+//   $('.input').on('keyup',function(){
+//       input= $('.input').val().toLowerCase();
+//       filter= $('[data-title]');
+//
+//
+//       if(input != ''){
+//         filter.hide();
+//         $('[data-title*="' + input +'"]').show();
+//       }else{
+//         filter.show();
+//       }
+//     });
 
 
-$('.input').on('keydown',function(){
+//JS code
 
-  test = $('.input').val();
-  test = test.toLowerCase();
-  console.log(test);
+  function search() {
 
-  // if(test = true){
-  //     $('li').show();
-  //   }else{
-  //     $('li').hide();
-  //   }
-});
+    let links;
+    let final;
+    let input = document.querySelector('.input');
+    let filter = input.value.toLowerCase();
+    let li = document.getElementsByTagName('li');
+
+
+    for (let i = 0; i < li.length; i++) {
+      links = li[i].getElementsByTagName("a")[0];
+      final = links.getAttribute('data-title');
+      if (final.toLowerCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
