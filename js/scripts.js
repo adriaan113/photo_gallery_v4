04eada803f25e232deea99647edefeae075a66a4
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-07-10 16:56:50 +0200

    added javascript/last version

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
 
-// Jquery code: i wanred to coe this challenge with jquery but could not figure out how to make
-//the [data-title] toLowerCase.
-
-// let input;
-// let filter;
-//
-//   $('.input').on('keyup',function(){
-//       input= $('.input').val().toLowerCase();
-//       filter= $('[data-title]');
-//
-//
-//       if(input != ''){
-//         filter.hide();
-//         $('[data-title*="' + input +'"]').show();
-//       }else{
-//         filter.show();
-//       }
-//     });
-
 
 //JS code
 
@@ -28,7 +9,6 @@
     let input = document.querySelector('.input');
     let filter = input.value.toLowerCase();
     let li = document.getElementsByTagName('li');
-
 
     for (let i = 0; i < li.length; i++) {
       links = li[i].getElementsByTagName("a")[0];
